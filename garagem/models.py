773a51f7223e9e96a307262c756649cc6ffb1b4a
jhenifer <jhenifermachado211@gmail.com>
2023-05-08 16:49:52 -0300

    8.4

--- garagem/models.py
+++ garagem/models.py
@@ -1,55 +1,58 @@
 from django.db import models
 
 
-class Categoria(models.Model):
-    descricao = models.CharField(max_length=50)
+class Acessorio(models.Model):
+
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.descricao
+        return self.descricao.title()
+
+    class Meta:
+        verbose_name_plural = "Acessórios"
+        verbose_name = "Acessório"
+
+
+class Categoria(models.Model):
+
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao.title()
+
+
+class Cor(models.Model):
+
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao.title()
+
+    class Meta:
+        verbose_name_plural = "Cores"
 
 
 class Marca(models.Model):
+
     nome = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.nome.upper()
 
 
-class Acessorio(models.Model):
-    descricao = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.descricao
-    
-    class Meta:
-        verbose_name_plural = "acessórios"
-        verbose_name = "acessório"
-
-
-class Cor(models.Model):
-    descricao = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.descricao
-    
-      
-    class Meta:
-        verbose_name_plural = "cores"
-        verbose_name = "cor"
-
-
 class Veiculo(models.Model):
-    modelo = models.CharField(max_length=100)
-    ano = models.IntegerField(default=0, null=True, blank=True)
-    preco = models.DecimalField( default = 0,null=True, blank=True, max_digits=10, decimal_places=2)
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT)
+    acessorios = models.ManyToManyField(Acessorio)
+    ano = models.IntegerField(null=True, blank=True, default=0)
+    preco = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True, default=0.0
+    )
 
     def __str__(self):
-        return f"{self.marca} {self.modelo} {self.ano}, {self.cor}"
-    
+        return f"{self.modelo.marca} {self.modelo.nome} {self.ano} {self.cor}"
+
     class Meta:
-        verbose_name_plural = "veículos"
-        verbose_name = "veículo"
+        verbose_name_plural = "Veículos"
+        verbose_name = "Veículo"
--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo, Modelo
+from garagem.serializers import AcessorioSerializer,CategoriaSerializer,CorSerializer,MarcaSerializer,ModeloSerializer,ModeloDetailSerializer,ModeloListSerializer,VeiculoSerializer,VeiculoListSerializer,VeiculoDetailSerializer
+
+
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+
+class ModeloViewSet(ModelViewSet):
+    queryset = Modelo.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ModeloListSerializer
+        elif self.action == "retrieve":
+            return ModeloDetailSerializer
+        return ModeloSerializer
+
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+
+    serializer_classes = {
+        "list": VeiculoListSerializer,
+        "retrieve": VeiculoDetailSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, VeiculoSerializer)
